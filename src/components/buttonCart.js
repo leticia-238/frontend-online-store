6fb1dce87deb021567c0620111a0be908799cf9e
Janiele Vilar <vilarjaniele@gmail.com>
2022-03-11 10:00:09 -0300

    Req. 3 sem erro de lint

--- src/components/buttonCart.js
+++ src/components/buttonCart.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 class ButtonCart extends React.Component {
   render() {
     return (
-      <Link data-testid="shopping-cart-button" to="/cart" >
+      <Link data-testid="shopping-cart-button" to="/cart">
         carrinho
       </Link>
-    )
+    );
   }
 }
 
