7173bd55a4ff5300b9e68e511a910b039d3b011f
Janiele Vilar <vilarjaniele@gmail.com>
2022-03-12 11:26:46 -0300

    buttonCart sem erro de lint

--- src/components/buttonCart.js
+++ src/components/buttonCart.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaShoppingCart } from 'react-icons/fa'
+import { FaShoppingCart } from 'react-icons/fa';
 
 class ButtonCart extends React.Component {
   render() {
     return (
-        <Link
-          data-testid="shopping-cart-button"
-          to="/cart"
-          className="buttonCart"
-        >
+      <Link
+        data-testid="shopping-cart-button"
+        to="/cart"
+        className="buttonCart"
+      >
         <FaShoppingCart />
       </Link>
     );
