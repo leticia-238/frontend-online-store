020ed1dc1c1b2661319b491cb5ac7943316c8c6a
Janiele Vilar <vilarjaniele@gmail.com>
2022-03-12 11:17:37 -0300

    Adiciona icone de carrinho

--- src/components/buttonCart.js
+++ src/components/buttonCart.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaShoppingCart } from 'react-icons/fa'
 
 class ButtonCart extends React.Component {
   render() {
     return (
-      <Link data-testid="shopping-cart-button" to="/cart">
-        carrinho
+        <Link
+          data-testid="shopping-cart-button"
+          to="/cart"
+          className="buttonCart"
+        >
+        <FaShoppingCart />
       </Link>
     );
   }
